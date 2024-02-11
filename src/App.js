75d6e59296e7ce41c5c5ae969a4c0d2d8e83dfd8
ARJUN AJITH <arjun@ARJUNs-MacBook-Pro.local>
2024-02-11 15:09:07 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -4,27 +4,13 @@ import Home from './components/Home/Home';
 import About from './components/About/About';
 import Team from './components/Team/Team';
 import Contact from './components/Contact/Contact';
-import AOS from 'aos';
 import 'aos/dist/aos.css';
 
-AOS.init();
 
 function App() {
   return (
     <div className="App">
-      <div>
       <NavBar />
-     <Home />
-      </div>
-     <div>
-     <About />
-     </div>
-     <div data-aos="fade-in" data-aos-delay="200">
-     <Team />
-     </div>
-     <div>
-     <Contact />
-     </div>
     </div>
   );
 }
