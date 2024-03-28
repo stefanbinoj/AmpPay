588ff383cb6c26e4f97d72b3a6a56af2ed0269f8
ARJUN AJITH <arjun@ARJUNs-MacBook-Pro.local>
2024-03-28 12:55:41 +0530

    fetch error fix

--- src/components/UserDashboard/Prediction.js
+++ src/components/UserDashboard/Prediction.js
@@ -76,7 +76,7 @@ useEffect(() => {
   };
 
   fetchData();
-  const interval = setInterval(fetchData,1000);
+  const interval = setInterval(fetchData,3000);
   return () => clearInterval(interval);
 }, []);
 
