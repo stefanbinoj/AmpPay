cea9a738c67d1f8be5a0ba6820c811cef50f4e71
ARJUN AJITH <arjun@ARJUNs-MacBook-Pro.local>
2024-04-06 09:15:50 +0530

    carousel component

---
+++ src/components/Carousel.js
@@ -0,0 +1,58 @@
+import Carousel from 'react-bootstrap/Carousel';
+import ExampleCarouselImage from "../images/IMG_20240125_122905.jpg"
+import ExampleCarouselImage1 from "../images/pexels-mikhail-nilov-6963032.jpg"
+import './Carousel.css'
+
+function IndividualIntervalsExample() {
+  return (
+    <Carousel nextIcon={<CustomNextButton />} prevIcon={<CustomPrevButton />}>
+      <Carousel.Item interval={2000}>
+        <img src={ExampleCarouselImage} alt="First slide" />
+        <Carousel.Caption>
+          <h3>Transforming Energy Habits</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={2000}>
+        <img src={ExampleCarouselImage1} alt="Second slide" />
+        <Carousel.Caption>
+          <h3>Transforming Energy Habits</h3>
+          <p>Turn the tide on high energy bills and make informed decisions about your consumption with AmpPay.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={2000}>
+        <img src={ExampleCarouselImage} alt="Third slide" />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+
+function CustomNextButton(props) {
+  return (
+    <button className="carousel-custom-next" onClick={props.onClick}>
+      <svg xmlns="http://www.w3.org/2000/svg" width="4em" height="4em" viewBox="0 0 24 24">
+        <path className='custom-button-path' fill="#2f2d2d" d="m13.172 12l-4.95-4.95l1.414-1.413L16 12l-6.364 6.364l-1.414-1.415z"/>
+      </svg>
+    </button>
+  );
+}
+
+
+function CustomPrevButton(props) {
+  return (
+    <button className="carousel-custom-prev" onClick={props.onClick}>
+      <svg xmlns="http://www.w3.org/2000/svg" width="4em" height="4em" viewBox="0 0 24 24">
+	      <path className='custom-button-path' fill="#2f2d2d" d="m10.828 12l4.95 4.95l-1.414 1.415L8 12l6.364-6.364l1.414 1.414z" />
+      </svg>
+    </button>
+  );
+}
+
+export default IndividualIntervalsExample;
