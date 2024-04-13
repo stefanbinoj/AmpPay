dfc62b1afc977c45bd1f5f3c4b456af00a9e52cf
ARJUN AJITH <arjun@ARJUNs-MacBook-Pro.local>
2024-04-14 01:57:57 +0530

    vision section/css

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,10 +2,12 @@ import Card from './Card';
 import './About.css';
 import Features from './Features';
 import Value from './Value';
+import Vision from './Vision';
 
 const Home = () => {
     return (
     <div id='about1' className='about-container'>   
+        <Vision />
         <Value />
         <Card /> 
         <Features />
---
+++ src/components/About/Vision.js
@@ -0,0 +1,38 @@
+import './Vision.css'
+import { useState,useEffect, useRef } from 'react';
+const Vision = () => {
+    const [visibleHeader, setVisibleHeader] = useState(false);
+    const sectionRef = useRef(null);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            (entries) => {
+                // Check if the section is intersecting with the viewport
+                if (entries[0].isIntersecting) {
+                    setVisibleHeader(true);
+                } else {
+                    setVisibleHeader(false);
+                }
+            },
+            { threshold: 0 } 
+        );
+        observer.observe(sectionRef.current);
+        return () => {
+            observer.disconnect();
+        };
+    }, []);
+    return (
+        <div id='vision' className="vision-container" ref={sectionRef}>
+            <div className='vision-inner'>
+          <header className="vision-header">
+            <p className={`vision-title ${visibleHeader ? 'active_header' : 'hidden'}`}>We’re AmpPay, Lighting the Way to Energy Efficiency</p>
+          </header>
+          <div className="vision-intro">
+            <p>Welcome to AmpPay – your gateway to empowered energy management. Founded by a team passionate about sustainability, AmpPay offers a comprehensive solution for tracking, managing, and optimizing energy consumption. What sets us apart is our commitment to innovation and our dedication to empowering users to make informed decisions about their energy usage. At AmpPay, we value sustainability, efficiency, and community, and our platform offers real-time tracking, predictive insights, and personalized recommendations to help you achieve your energy goals. Explore AmpPay today and take control of your energy consumption like never before.</p>
+          </div>
+          </div>
+        </div>
+      );
+}
+
+export default Vision
